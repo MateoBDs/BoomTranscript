be8f88dbfe14e4807fed86e0051c00e23dca3e11
MateoBD <mateobeldiaz@gmail.com>
2026-07-28 19:23:23 +0100

    Implement transcript command for Discord bot
    Add a Discord bot command to generate transcripts.

--- examples/test_bot.py
+++ examples/test_bot.py
@@ -1,1 +1,30 @@
+import discord
+from discord.ext import commands
 
+from boomtranscript import TranscriptExporter
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+
+@bot.command()
+async def transcript(ctx):
+
+    exporter = TranscriptExporter(ctx.channel)
+
+    file = await exporter.export()
+
+    await ctx.send(
+        "Transcript generado:",
+        file=discord.File(file)
+    )
+
+
+bot.run("TOKEN")
